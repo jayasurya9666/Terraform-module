bf8df2c62cca5110aecab1fdba500a4e77dc0973
jayasurya9666 <68177811+jayasurya9666@users.noreply.github.com>
2021-06-30 11:45:43 +0530

    Add files via upload

---
+++ main(module).tf
@@ -0,0 +1,23 @@
+provider "aws" {
+    access_key = "AKIA2O3EDZYGBO32IMWD"
+    secret_key = "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"
+    region     = "us-east-1"
+}
+
+module "Dev" {
+
+    source ="./modules/common"
+    vpc_name ="Terraform-VPC"
+    vpc_cidr = "10.1.0.0/16"
+    IGW_name ="Terraform-IGW"
+    key_name ="DEMO"
+    public_subnet1_cidr ="10.1.1.0/24"
+    public_subnet2_cidr ="10.1.2.0/24"
+    public_subnet3_cidr ="10.1.3.0/24"
+    public_subnet1_name ="Terraform-Subnet-1"
+    public_subnet2_name ="Terraform-Subnet-1"
+    public_subnet3_name ="Terraform-Subnet-1"
+    Main_Routing_Table ="Terraform-Route"
+
+  
+}
